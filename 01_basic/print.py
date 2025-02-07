623e3e8720c1a7330758f29dac31d1ee7927f2ec
Fran-TP <ftorresp@unasam.edu.pe>
2025-02-06 23:31:28 -0500

    initialize project structure and add basic print examples

---
+++ 01_basic/print.py
@@ -0,0 +1,27 @@
+# basic print
+print("Hello World from Python")
+
+# basic print using single quotes
+print('Hello World from Python')
+
+# multiple prints
+print("hello", "world", "from", "python")
+
+# print with separator
+print("hello", "world", "from", "python", sep="-")
+
+# print using end
+print("Fran", "Torres", end="\n")
+print("Another line")
+
+# with double quotes and single quotes
+print("Hello 'World' from Python")
+
+# with single quotes and double quotes
+print('Hello "World" from Python')
+
+# with backslash and double quotes
+print("Hello \"World\" from Python")
+
+# with docstring
+print("""Hello World from Python""")
