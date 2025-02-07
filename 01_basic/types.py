f549e84495b932f5bc5d7e477f3b1f57b4531344
Fran-TP <ftorresp@unasam.edu.pe>
2025-02-06 23:51:31 -0500

    add project configuration files and basic type examples

---
+++ 01_basic/types.py
@@ -0,0 +1,30 @@
+"""
+types in python
+"""
+# int
+print(type(1))
+print(type(-0))
+print(type(0))
+print(type(123141231551231521343242))
+
+# float
+print(type(1.0))
+print(type(1e4))
+print(type(1.0e-4))
+
+# complex
+print(type(1+1j))
+print(type(1.0+1.0j))
+
+# str
+print(type('hello'))
+print(type("hello"))
+print(type('''hello'''))
+print(type("""hello"""))
+
+# bool
+print(type(True))
+print(type(False))
+
+# None
+print(type(None))
