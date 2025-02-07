6281a574d9bf18c132f5c9a1a2f1878c6f7e326d
Fran-TP <ftorresp@unasam.edu.pe>
2025-02-06 23:55:53 -0500

    add project configuration files and basic type examples, including type casting script

---
+++ 01_basic/cast.py
@@ -0,0 +1,14 @@
+"""
+ casting types
+"""
+
+# casting types
+print(int(3.14))
+print(3 + int("100"))
+print(float(3))
+print(str(3.14))
+print(bool(0))
+print(bool(1))
+print(bool(3.14))
+print(bool(''))
+print(bool(' '))
