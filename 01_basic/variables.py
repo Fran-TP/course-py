3be339523539e597bb8760005b5567dc51a92458
Fran-TP <ftorresp@unasam.edu.pe>
2025-02-07 00:00:37 -0500

    add variables examples

---
+++ 01_basic/variables.py
@@ -0,0 +1,24 @@
+"""
+variables
+"""
+
+name = "John"
+age = 25
+height = 6.1
+is_adult = True
+
+print(name)
+print(age)
+print(height)
+print(is_adult)
+
+# const
+PI = 3.14
+ANY_CONSTANT = 100
+
+# with annotation
+name: str = "John"
+is_user_adult: bool = True
+
+# print using f-string
+print(f"Name: {name} Age: {age} Height: {height} Adult: {is_adult}")
