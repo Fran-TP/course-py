159bc51d881b49ddcb49abf1cf853af267f94a9c
Fran-TP <ftorresp@unasam.edu.pe>
2025-02-07 23:36:07 -0500

    add invoices page

---
+++ 01_basic/exercices.py
@@ -0,0 +1,10 @@
+print("\nEjercicio 2: Muestra los tipos de datos de las siguientes variables:")
+print("Usa el comando 'type()' para determinar el tipo de datos de cada variable.")
+a = 15
+b = 3.14159
+c = "Hola mundo"
+d = True
+e = None
+
+print([type(a), type(b), type(c), type(d), type(e)])
+
