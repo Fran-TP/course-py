9e43bad5fe30e60b293be9b24a529e337cb5de0c
Fran-TP <ftorresp@unasam.edu.pe>
2025-02-08 21:13:11 -0500

    add list in py

---
+++ 02_flow_control/list.py
@@ -0,0 +1,29 @@
+"""
+List in Python
+"""
+# List of integers
+my_list = [1,2,3,4,5]
+
+# list of strings
+my_list_str = ['apple', 'banana', 'cherry']
+
+# list of mixed data types
+my_list_mixed = [1, 'apple', 2, 'banana']
+
+# Empty list
+empty_list = []
+
+# Nested list
+nested_list = [['apple', 'banana'], [1, 2, 3]]
+
+# Matrix list
+Matrix_list = [[1,2], [3,4], [5,6]]
+
+# Accessing elements in a list
+print(my_list[0])  # 1
+print(my_list[1])  # 2
+print(my_list[-1])  # 5
+
+# Slicing a list
+print(my_list[1:3])  # [2, 3]
+print(my_list[::-1])  # [1, 3]
